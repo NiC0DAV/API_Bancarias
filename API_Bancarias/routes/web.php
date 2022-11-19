83f8740635bbc299dd254bf67d00c01129f22b0d
Johan Castro <johan.castro@INFODEC.DEV>
2022-11-19 16:36:55 -0500

    Change structure of routes;

--- API_Bancarias/routes/web.php
+++ API_Bancarias/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('createClient', 'App\Http\Controllers\UserController@createClient');
-
-Route::post('credit/authenticate', 'App\Http\Controllers\UserController@MSCusBilCredAuthenticateEF');
+Route::post('createClient', [UserController::class, 'createClient'])->name('createClient');
+Route::post('credit/authenticate', [UserController::class, 'MSCusBilCredAuthenticateEF'])->name('MSCusBilCredAuthenticateEF');
