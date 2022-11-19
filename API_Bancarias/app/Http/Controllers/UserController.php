a3762b6168fff72c01ffad50353c0ed6a80599be
NicolasEstevez <esteveznicolas0@gmail.com>
2022-11-19 17:49:19 -0500

    Carga funcionalidad de autenticacion y respectivo middleware para las rutas

--- API_Bancarias/app/Http/Controllers/UserController.php
+++ API_Bancarias/app/Http/Controllers/UserController.php
@@ -6,13 +6,14 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\User;
 use App\Helpers\JwtAuth;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
 
     public function createClient(Request $request)
     {
-        $clientName = $request->input('cliente');
+        $clientName = $request->input('cliente', null);
         $unixTime = strtotime('now');
         $clientId = md5(uniqid($unixTime . $clientName, true));
         $clientSecret = md5(uniqid($unixTime . $clientName, true));
@@ -35,8 +36,70 @@ class UserController extends Controller
         return response()->json($response, $response['code']);
     }
 
-    public function MSCusBilCredAuthenticateEF()
+    public function MSCusBilCredAuthenticateEF(Request $request)
     {
         $jwtAuth = new JwtAuth();
+        $clientId = $request->input('clientId', null);
+        $clientSecret = $request->input('clientSecret', null);
+        $paramsArr = ['clientId' => $clientId, 'clientSecret' => $clientSecret];
+
+        $checkClient = User::where([
+            'clientId' => $clientId,
+            'clientSecret' => $clientSecret,
+        ])->first();
+
+        // $response = array(
+        //     'status' => 'Success',
+        //     'code' => 200,
+        //     'message' => 'El usuario ha sido identificado exitosamente.',
+        //     'data' => $checkClient
+        // );
+
+        // return response()->json($response, $response['code']);
+        // die();
+        if ($checkClient != null && !empty($checkClient)) {
+
+            $validate = Validator::make($paramsArr, [
+                'clientId' => 'required',
+                'clientSecret' => 'required'
+            ]);
+
+            if ($validate->fails()) {
+                $response = array(
+                    'status' => 'Error',
+                    'code' => 404,
+                    'message' => 'El cliente no se ha podido identificar o los datos ingresados no son validos',
+                    'data' => $validate->errors()
+                );
+            } else {
+
+                $signUp = $jwtAuth->loginClient($paramsArr['clientId'], $paramsArr['clientSecret']);
+
+                if (!empty($signUp) && $signUp != '') {
+                    $response = array(
+                        'status' => 'Success',
+                        'code' => 200,
+                        'message' => 'El usuario ha sido identificado exitosamente.',
+                        'data' => $signUp
+                    );
+                } else {
+                    $response = array(
+                        'status' => 'Error',
+                        'code' => 500,
+                        'message' => 'Ocurrio un error mientras el usuario iniciaba sesión.',
+                        'data' => ''
+                    );
+                }
+            }
+        } else {
+            $response = array(
+                'status' => 'Error',
+                'code' => 404,
+                'message' => 'El cliente con el que intenta autenticarse no existe en el sistema.',
+                'data' => ''
+            );
+        }
+
+        return response()->json($response, $response['code']);
     }
 }
---
+++ API_Bancarias/app/Http/Middleware/ApiAuthMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Helpers\JwtAuth;
+use Closure;
+use Illuminate\Http\Request;
+
+class ApiAuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $tokenAuth = $request->header('Authorization');
+        $jwtValidator = new JwtAuth();
+        $checkToken = $jwtValidator->checkToken($tokenAuth);
+
+        if ($checkToken) {
+            return $next($request);
+        } else {
+
+            $data = array(
+                'code' => 400,
+                'status' => 'Error',
+                'message' => 'Error el usuario no esta identificado'
+            );
+
+            return response()->json($data, $data['code']);
+        }
+    }
+}
