6e70fec839f984e36b00ef7560a4372ac73f2a0f
Johan Castro <johan.castro@INFODEC.DEV>
2022-11-19 17:52:26 -0500

    Create service MSCusBilCredSimulateEF

---
+++ API_Bancarias/app/Http/Requests/MSCusBilCredSimulateEFRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MSCusBilCredSimulateEFRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'financialCode'           =>    'required',
+            'totalAmount'             =>    'required|numeric',
+            'shippingAmount'          =>    'required|numeric',
+            'totalTaxesAmount'        =>    'required|numeric',
+            'currency'                =>    'required',
+            'documentType'            =>    'required|numeric',
+            'documentNumber'          =>    'required',
+            'firstName'               =>    'required',
+            'lastName'                =>    'required',
+            'email'                   =>    'required|email',
+            'mobileNumber'            =>    'required',
+            'mobileNumberCountryCode' =>    'required'
+        ];
+    }
+}
